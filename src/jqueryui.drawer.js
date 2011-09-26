99569c5e665be25348cd4b9b3f23d251b568f8df
Andrew Couch <andy@couchand.com>
2011-09-26 08:11:16 -0400

    removing WIP

--- src/jqueryui.drawer.js
+++ src/jqueryui.drawer.js
@@ -8,11 +8,6 @@
  */
 (function($){
 	$.widget( 'ui.drawer', {
-/*
-		options: {
-			position: 'top left'
-		},
-*/
 		_create: function(){
 
 var	self		= this,
@@ -21,7 +16,6 @@ var	self		= this,
 	$drawer		= $container	.wrap(	'<div class="ui-widget ui-drawer"></div>'	).parent(),
 	$handle		= $('<div class="ui-widget-header ui-drawer-handle"></div>'		).appendTo( $container	),
 	$handleIcon	= $('<div class="ui-icon"></div>'					).appendTo( $handle	);
-//	$moveIcon	= $handle	.append('<div class="ui-drawer-move ui-icon ui-icon-arrowthick-2-ne-sw"></div>'	).children().last();
 
 $contents	.addClass( 'ui-widget-content ui-drawer-content ui-corner-br' );
 $handle		.addClass( 'ui-corner-tr ui-corner-br' );
@@ -38,51 +32,14 @@ $handleIcon	.addClass( 'ui-icon ui-icon-gear' )
 			}
 
 		});
-/*
-$drawer.draggable({
-	handle:		'.ui-drawer-move',
-	snap:		'body',
-	snapMode:	'inner',
-	start: function(){
 
-		//$moveIcon.removeClass('ui-state-default').addClass('ui-state-active');
-
-		//$drawer		.wrap(	'<div class="ui-cover ui-tl-cover"></div>' );
-		$('body')	.append('<div class="ui-cover ui-tr-cover"></div>' ).find('.ui-tr-cover').droppable({
-			accept:		'.ui-drawer',
-			hoverClass:	'ui-cover-active',
-			drop:		function(){
-						alert('hgeere');
-					}
-		});
-		$('body')	.append('<div class="ui-cover ui-bl-cover"></div>' ).find('.ui-bl-cover').droppable({
-			accept:		'.ui-drawer',
-			hoverClass:	'ui-cover-active',
-			drop:		function(){
-						alert('hgeere');
-					}
-		});
-		$('body')	.append('<div class="ui-cover ui-br-cover"></div>' ).find('.ui-br-cover').droppable({
-			accept:		'.ui-drawer',
-			hoverClass:	'ui-cover-active',
-			drop:		function(){
-						alert('hgeere');
-					}
-		});
-
-	}
-});
-*/
 $drawer.css('left', -1 * $container.width());
 
 		},
 
 		_setOption: function( key, value ){
-/*switch( key ) {
-	case "position":
-		break;
-}*/
-this._super( "_setOption", key, value );
+
+			this._super( "_setOption", key, value );
 
 		}
 
